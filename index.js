3cc105518bee7876bb233cf464c944a67eb64dbc
duongthaison21 <duongthaison21@gmail.com>
2022-07-13 00:09:12 +0700

    Add template engine

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const router = require('./src/routes/router');
+const path = require('path');
 const express = require('express');
-const app = express();
-const port = 3000;
+const morgan = require('morgan');
+const helmet = require('helmet');
+const handlebars = require('express-handlebars');
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Welcome');
-})
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(morgan('combined'));
+
+router(app);
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 })
